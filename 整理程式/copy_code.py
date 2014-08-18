38a9c20f7f521c8de2acd064dcda3b4800d5793d
JengYiShen <jeng2604@gmail.com>
2014-08-18 16:26:43 +0800

    copy_code v1

---
+++ 整理程式/copy_code.py
@@ -0,0 +1,35 @@
+﻿import os
+
+def 檔案複製():
+	列表 = open('test.txt',encoding='UTF-8')
+	檔案成員 = 列表.readlines()
+	列表.close()
+	
+	
+	字典 = dict()
+	
+	
+	for 成員 in 檔案成員:
+		處理過的成員 = 成員.strip()
+		字典[os.path.basename(處理過的成員)] = 處理過的成員
+		
+	print (字典)
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+if __name__ == '__main__':
+		檔案複製()
