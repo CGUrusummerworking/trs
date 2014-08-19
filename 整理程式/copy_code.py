83ff081aa88cfde2b929c82b02c00b1552f908e9
JengYiShen <jeng2604@gmail.com>
2014-08-19 16:45:09 +0800

    發生.git key error問題

--- 整理程式/copy_code.py
+++ 整理程式/copy_code.py
@@ -1,10 +1,8 @@
 ﻿import os
-import sys
-from os import listdir
-from os.path import isfile, join
+import shutil
 
 def 檔案複製():
-	列表 = open('test.txt',encoding='UTF-8')
+	列表 = open('file_list.txt',encoding='UTF-8')
 	檔案成員 = 列表.readlines()
 	列表.close()
 	
@@ -14,33 +12,29 @@ def 檔案複製():
 	
 	for 成員 in 檔案成員:
 		處理過的成員 = 成員.strip()
-		字典[os.path.basename(處理過的成員)] = 處理過的成員
-		
-	#print (字典)
-	位置 = "C:/Users/jeng/git/trs"
+		檔名 = os.path.basename(處理過的成員)
+		所屬資料夾 = 處理過的成員.strip(檔名)
+		字典[os.path.basename(處理過的成員)] = 所屬資料夾
+
+	
+	
+	位置 = "C:/Users/jeng/git/trs/"
 	開啟的位置 = os.listdir(位置)
 	
+	
 	for 成員 in 開啟的位置:
-		print(成員)
+		#print(字典[成員])
+		try:
+			目標位置= 位置  + 字典[成員]
+			if (not(os.path.exists(目標位置))):
+				os.makedirs(目標位置)
+			shutil.copy(位置 + '/' + 成員,目標位置  + 成員)
+		except:
+			print(字典[成員])
 
-	
 
-		
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 if __name__ == '__main__':
 		檔案複製()
+		
