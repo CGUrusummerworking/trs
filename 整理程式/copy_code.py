c5d88f8b05d6fef297491cc5865709627ee0d831
JengYiShen <jeng2604@gmail.com>
2014-08-19 11:56:02 +0800

    fix #2

--- 整理程式/copy_code.py
+++ 整理程式/copy_code.py
@@ -1,4 +1,7 @@
 ﻿import os
+import sys
+from os import listdir
+from os.path import isfile, join
 
 def 檔案複製():
 	列表 = open('test.txt',encoding='UTF-8')
@@ -13,7 +16,15 @@ def 檔案複製():
 		處理過的成員 = 成員.strip()
 		字典[os.path.basename(處理過的成員)] = 處理過的成員
 		
-	print (字典)
+	#print (字典)
+	位置 = "C:/Users/jeng/git/trs"
+	開啟的位置 = os.listdir(位置)
+	
+	for 成員 in 開啟的位置:
+		print(成員)
+
+	
+
 		
 		
 	
